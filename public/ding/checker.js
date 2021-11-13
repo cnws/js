a84c319d8d2024aa4dc4448e512cba359e65fdb5
senioi <93489820+cnws@users.noreply.github.com>
2021-11-13 18:47:34 +0800

    Update checker.js

--- public/ding/checker.js
+++ public/ding/checker.js
@@ -1,46 +1,60 @@
 document.querySelector("#CheckId").addEventListener("submit", (e) => {
-e.preventDefault();
-history.replaceState && history.replaceState(
-  null, '', location.pathname + location.search.replace(/[\?&]__cf.*=[^&]+/, '').replace(/^&/, '?') + location.hash
-);
-let Id = document.querySelector("#SId").value;
-if (Id === "") {
-document.querySelector(".sapi").innerHTML = `
+  e.preventDefault();
+  history.replaceState &&
+    history.replaceState(
+      null,
+      "",
+      location.pathname +
+        location.search.replace(/[\?&]__cf.*=[^&]+/, "").replace(/^&/, "?") +
+        location.hash
+    );
+  let Id = document.querySelector("#SId").value;
+  if (Id === "") {
+    document.querySelector(".sapi").innerHTML = `
 <div class="alert alert-danger alert-dismissable text-center">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <strong>请输入AppId或商店链接！</strong>
 </div>`;
-return;
-}
-fetch(window.location.origin + "/CheckId", {
-method: "POST",
-headers: {
-Accept: "application/json, text/plain, */*",
-"Content-type": "application/json; charset=utf-8",
-},
-body: JSON.stringify({
-Id,
-}),
-})
-.then((data) => console.log(data))
-.then((res) => res.json())
-//.then((data) => console.log(data))
-.then((ret) => {
-$("#SId").val('');
-if (ret.Data.ErrMsg !== null) {
-document.querySelector(".sapi").innerHTML = `
+    return;
+  }
+  fetch(window.location.origin + "/CheckId", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({
+      Id,
+    }),
+  })
+    //.then((data) => console.log(data))
+    //.then((res) => res.json())
+    //.then((data) => console.log(data))
+    .then((res) => {
+      if (error.status !== 200) {
+        ocument.querySelector(".sapi").innerHTML = `
+<div class="alert alert-danger alert-dismissable">
+<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+错误：<strong>访问超时，请重新刷新该页面！ </strong>
+</div>`;
+        return;
+      }
+      let ret = res.json();
+      $("#SId").val("");
+      if (ret.Data.ErrMsg !== null) {
+        document.querySelector(".sapi").innerHTML = `
 <div class="alert alert-danger alert-dismissable text-center">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <strong>${ret.Data.ErrMsg} </strong>
 </div>`;
-} else if (ret.Data.Id == 0) {
-document.querySelector(".sapi").innerHTML = `
+      } else if (ret.Data.Id == 0) {
+        document.querySelector(".sapi").innerHTML = `
 <div class="alert alert-danger alert-warning text-center">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <strong>公共库不存在：</strong> ${Id}
 </div>`;
-} else {
-document.querySelector(".sapi").innerHTML = `
+      } else {
+        document.querySelector(".sapi").innerHTML = `
 <table border="0" cellspacing="0" cellpadding="0" class="table">
     <tr class=" label-primary">
         <th scope="col" width="45%"><span style="color:white">AppId</span></th>
@@ -60,7 +74,19 @@ document.querySelector(".sapi").innerHTML = `
     </tr>
 </table>
 `;
-}
-})
-.catch((err) => console.log(err));
-})
+      }
+    })
+    //.catch((err) => console.log(err));
+    .catch(function (error) {
+      if (error.status !== 200) {
+        ocument.querySelector(".sapi").innerHTML = `
+<div class="alert alert-danger alert-dismissable">
+<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+错误：<strong>访问超时，请重新刷新该页面！ </strong>
+</div>`;
+        return;
+      } else {
+        notifyError(error.data.detail);
+      }
+    });
+});
